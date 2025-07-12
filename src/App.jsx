6e7905bb49b368f46deac6ffc0620328d62911b2
Jahid-HasanM <web.businessp@gmail.com>
2025-07-11 23:56:51 -0700

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import "./App.css"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <div className="nav_bar">
+        <a href="#"><img src="./src/assets/react.svg" alt="Logo" />React</a>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="">Service</a></li>
+        </ul>
+      </div>
+    </>
   )
 }
 
